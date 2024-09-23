56bf56fe10f1e58784e0e18a278f809df984ee10
willen01 <willen.sesantos@gmail.com>
2024-09-23 17:27:59 -0300

    primeiro commit

---
+++ proposta-app/src/main/java/com/willen/proposta_app/PropostaAppApplication.java
@@ -0,0 +1,13 @@
+package com.willen.proposta_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaAppApplication.class, args);
+	}
+
+}
---
+++ proposta-app/src/test/java/com/willen/proposta_app/PropostaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willen.proposta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
